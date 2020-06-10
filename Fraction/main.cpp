78c5de34fec840aab34595b92a8df467c653b52f
httpc <makshttpc@gmail.com>
2020-06-10 18:14:27 +0300

    hime

---
+++ Fraction/main.cpp
@@ -0,0 +1,171 @@
+#include<iostream>
+using namespace std;
+
+class Fraction
+{
+	int integer;	//Целая часть
+	int numerator;	//Числитель
+	int denominator;//Знаменатель
+public:
+	const int get_integer()const
+	{
+		return integer;
+	}
+	const int get_numerator()const
+	{
+		return numerator;
+	}
+	const int get_denominator()const
+	{
+		return denominator;
+	}
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1;
+		this->denominator = denominator;
+	}
+
+
+	//		Constructors:
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		this->set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		this->set_denominator(denominator);
+		cout << "Constructor:\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyConstructor:" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//		Operators:
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssignment: " << this << endl;
+		return *this;
+	}
+
+	//		Methods:
+	void print()const
+	{
+		if (integer)cout << integer;
+		if (numerator)
+		{
+			if (integer)cout << "+";
+			//if (integer)cout << "(";
+			cout << numerator << "/" << denominator;
+			//if (integer)cout << ")";
+		}
+		else if (integer == 0)
+			cout << 0;
+		cout << endl;
+	}
+	void to_proper()
+	{
+		while (numerator >= denominator)
+		{
+			integer++;
+			numerator -= denominator;
+		}
+	}
+	void to_improper()
+	{
+		while (integer > 0)
+		{
+			integer--;
+			numerator += denominator;
+		}
+	}
+	void reduce()
+	{
+		while (true)
+		{
+			if ((denominator % numerator) == 0)
+			{
+				int temp = numerator;
+				denominator /= temp;
+				numerator /= temp;
+			}
+			if (numerator == denominator)
+			{
+				numerator = 0;
+				denominator = 0;
+				integer++;
+			}
+			break;
+		}
+	}
+};
+
+//#define CONSTRUCTORS_CHECK
+
+void main()
+{
+#ifdef CONSTRUCTORS_CHECK
+	Fraction A;
+	A.print();
+	Fraction B = 3;
+	B.print();
+	Fraction C(4, 5);
+	C.print();
+	Fraction D(5, 4, 7);
+	Fraction E = D;//CopyConstructor
+	E.print();
+	Fraction F;		//DefaultConstructor
+	F = E;
+	F.print();
+#endif // CONSTRUCTORS_CHECK
+
+	/*Fraction A(1, 2);
+	A.print();
+	Fraction B;
+	B.print();
+	cout << "\n-------------------------------------------\n";
+	B = A;
+	cout << "\n-------------------------------------------\n";*/
+	int a=0, b=3, c=6;
+	Fraction A(a, b, c);
+	A.print();
+	A.reduce();
+	A.print();
+}
